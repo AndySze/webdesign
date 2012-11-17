d6cec75b7b570edc9ee48be369d0b90c1de07792
AndySze <andy.sze.mail@gmail.com>
2012-11-17 20:00:04 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cunlin::Application.config.secret_token = 'b6a11487be6a3826f843d66d377f783c35f7ba71b84b151bcf6d891fa2ab74ae01b46fcb38e8d625e583bf7dd7b07e3c0e2b85f57d19e15f6cf691be2b847549'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cunlin::Application.config.session_store :cookie_store, key: '_cunlin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cunlin::Application.config.session_store :active_record_store
